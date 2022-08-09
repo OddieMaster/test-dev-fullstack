7185f2ca51be9210b086060331703de18899d6fe
Guilherme Eduardo <romualdo.gui@gmail.com>
2022-08-09 19:18:10 -0300

    feat(backend, general): Insert cors policy

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -19,6 +19,14 @@ appApi.get("/", function (req, res) {
     res.send("hello world");
 });
 
+patients.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 /* ROTA PATIENTS */
 
 patients.get("/:idPatient", async (req, res) => {
